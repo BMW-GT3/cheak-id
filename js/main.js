badba65c2593b3da2b38e47c37237150f528642b
Alexandr <94461244+BMW-GT3@users.noreply.github.com>
2022-02-23 11:20:26 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let value = +prompt("@BMW.GT3, введите Ваш 4-значный PIN", '');
+
+switch (value){
+	case 1000:
+		alert('Введите в поисковике "BMW.GT3"');
+	break;
+	default:
+		alert('Неверно! Обновите страницу');
+}s
---
+++ js/main1.js
@@ -0,0 +1,9 @@
+let value = +prompt("@Jack.3, введите Ваш 4-значный PIN", '');
+
+switch (value){
+	case 2708:
+		alert('Введите в поисковике "Jack.3"');
+	break;
+	default:
+		alert('Неверно! Обновите страницу');
+}
